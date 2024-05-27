ca23d323979462e9962b8bcc1d13d9bcf1a34dfb
Madura <624759+manushanga@users.noreply.github.com>
2024-05-27 19:54:36 +0930

    Fix cmake

--- simple_main.cpp
+++ simple_main.cpp
@@ -1,3 +1,5 @@
+#include "component.h"
+#include "componentmanager.h"
 #include "eq.h"
 #include "ffmpeg.h"
 #include "metadata.h"
@@ -6,8 +8,9 @@
 #include "pulse.h"
 #include "resampler.h"
 #include "threadpool.h"
+#include <cassert>
 using namespace vrok;
-
+float BandValues[] = {5.0, 4.0, 3.0, 0.0, -0.2, -0.3, -0.5, -0.4, -0.3, -0.2, -0.1, 0.0, 0.0, 1.0, 2.0, 3.0, 3.0 };
 Player player;
 Resampler resampler;
 EffectSSEQ eq;
@@ -46,6 +49,14 @@ void Setup() {
     resampler.Preallocate();
     out.Preallocate();
 
+    Component* comp = ComponentManager::GetSingleton()->GetComponent("ShibatchSuperEQ:0");
+    for (int i=0;i<sizeof(BandValues)/sizeof(float);i++)  {
+        char bandNameBuf[16];
+        snprintf(bandNameBuf, 16, "Band%02d", i);
+        PropertyBase* prop = ComponentManager::GetSingleton()->GetProperty(comp, bandNameBuf);
+        ComponentManager::GetSingleton()->SetProperty(comp, prop, std::to_string(BandValues[i]));
+    }
+    
     pool.RegisterWork(0, &player);
     pool.RegisterWork(1, &eq);
     pool.RegisterWork(0, &resampler);
