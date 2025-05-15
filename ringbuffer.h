0ed03aa6e4f70be6e08e4c177096bd2d08ea8144
madura <madura@localhost.localdomain>
2025-05-15 23:42:49 +0930

    Bug fix

--- ringbuffer.h
+++ ringbuffer.h
@@ -22,6 +22,7 @@
 #include "util/mutil.h"
 #include <atomic>
 #include <cstdlib>
+#include <mutex>
 
 template <typename T>
 class Ringbuffer {
@@ -29,14 +30,15 @@ private:
     T *_buffer;
     size_t _size, _front, _rear;
     std::atomic<size_t> _used;
-
+    mutable std::mutex mutex;
 public:
     Ringbuffer(size_t size) : _size(size), _front(0), _rear(0), _used(0) {
         _buffer = (T *)mutil_aligned_alloc(size * sizeof(T));
     }
-    inline bool IsWritable(size_t n) const { return (_used + n <= _size); }
-    inline bool IsReadable(size_t n) const { return (n <= _used); }
+    inline bool IsWritable(size_t n) const { std::lock_guard<std::mutex> lg(mutex); return (_used + n <= _size); }
+    inline bool IsReadable(size_t n) const { std::lock_guard<std::mutex> lg(mutex); return (n <= _used); }
     inline bool Write(T *source, size_t n) {
+        std::lock_guard<std::mutex> lg(mutex);
         if (_used.load(std::memory_order_acquire) + n <= _size) {
             for (size_t i = 0; i < n; i++) {
                 _buffer[(_front + i) % _size] = source[i];
@@ -50,6 +52,7 @@ public:
     }
 
     inline bool Read(T *dest, size_t n) {
+        std::lock_guard<std::mutex> lg(mutex);
         if (n <= _used.load(std::memory_order_acquire)) {
             for (size_t i = 0; i < n; i++) {
                 dest[i] = _buffer[(_rear + i) % _size];
@@ -64,18 +67,20 @@ public:
     // Dest must be the same size as _size
     inline size_t ReadAvailable(T *dest, size_t n) {
         if (!Read(dest, n)) {
+            std::lock_guard<std::mutex> lg(mutex);
             size_t nused = _used.load(std::memory_order_acquire);
             
-            for (size_t i = 0; i < n; i++) {
+            for (size_t i = 0; i < nused; i++) {
                 dest[i] = _buffer[(_rear + i) % _size];
             }
-            _rear = (_rear + n) % _size;
+            _rear = (_rear + nused) % _size;
             _used.fetch_add(-nused, std::memory_order_release);
 
             return nused;
         }
         return n;
     }
+
     void Clear() {
         _front = 0;
         _rear = 0;
